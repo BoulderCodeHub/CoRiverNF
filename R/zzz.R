d9b01041c86bde74d3f75e7091212e575ec86ba0
Alan Butler <rabutler@usbr.gov>
2017-09-06 15:57:37 +0000

    startup message reflects the latest data

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 #' @import xts
 
 .onAttach <- function(libname, pkgname){
-  packageStartupMessage('CoRiverNF package currently includes 1906-2014 natural flows, published September 20, 2016.')
+  packageStartupMessage('CoRiverNF package currently includes 1906-2015 natural flows, released August 14, 2017.')
   Sys.setenv(TZ='UTC')
 }
