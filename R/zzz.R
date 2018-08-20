0c6ee8bec388bcf9f821d996a7960edcd3ade3e6
Alan Butler <rabutler@usbr.gov>
2018-08-19 22:05:32 -0600

    added a release question

--- R/zzz.R
+++ R/zzz.R
@@ -4,3 +4,10 @@
   packageStartupMessage('CoRiverNF package currently includes 1906-2015 natural flows, released August 14, 2017.')
   Sys.setenv(TZ='UTC')
 }
+
+release_questions <- function() 
+{
+  c(
+    "Did you run CRSSIO tests with this new package?"
+  )
+}
